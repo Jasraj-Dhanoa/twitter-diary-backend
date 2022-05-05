4eb109de42d8d8ca804499cccb86466f30eab3c4
Jasraj Dhanoa <jdhanoa@opentext.com>
2022-05-05 13:58:52 -0400

    Initial Commit

---
+++ src/main/java/com/example/twitterjournalbackend/TwitterJournalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.twitterjournalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterJournalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterJournalBackendApplication.class, args);
+	}
+
+}
