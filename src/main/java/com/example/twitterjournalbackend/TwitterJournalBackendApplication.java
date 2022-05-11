eedab6a399d99f8a62400aaf97edfb5adfebf087
Jasraj Dhanoa <jdhanoa@opentext.com>
2022-05-11 16:06:05 -0400

    provide front-end permission to fetch backend endpoints

--- src/main/java/com/example/twitterjournalbackend/TwitterJournalBackendApplication.java
+++ src/main/java/com/example/twitterjournalbackend/TwitterJournalBackendApplication.java
@@ -2,11 +2,24 @@ package com.example.twitterjournalbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TwitterJournalBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TwitterJournalBackendApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer configureCors() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 }
